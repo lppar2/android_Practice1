82267412827576d079abf1e2cac4529faad01300
lppar2 <lppar2@gmail.com>
2022-03-01 17:55:34 +0300

    Initial commit

---
+++ layouttype/src/main/java/ru/mirea/galkina/layouttype/MainActivity.java
@@ -0,0 +1,25 @@
+package ru.mirea.galkina.layouttype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView myTextView = (TextView) findViewById(R.id.editTextTextPersonName);
+
+        myTextView.setText("New text in MIREA");
+        Button button = findViewById(R.id.button26);
+        button.setText("MireaButton");
+
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setChecked(true);
+    }
+}
